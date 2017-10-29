d291ec27380ab236329ddda498f2b3a89d74f377
Ryan McFadden <rjmcfadd@gmail.com>
2017-10-29 17:57:05 -0400

    finished lesson-01

--- dev-profile/ViewController.swift
+++ dev-profile/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileIcon: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        profileIcon.layer.cornerRadius = 8
+        profileIcon.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
